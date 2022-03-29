2ac68a8dffaf71d15cbe62af4a4bd4952ce0e02f
Anastasiia Huhtala <anastasiia.huh@gmail.com>
2022-03-29 14:52:42 +0300

    basic components

--- app/src/main/java/com/example/mdp7/MainActivity.kt
+++ app/src/main/java/com/example/mdp7/MainActivity.kt
@@ -3,36 +3,81 @@ package com.example.mdp7
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mdp7.ui.theme.MDP7Theme
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.TextFieldValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MDP7Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.budgettracker),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .width(250.dp)
+                .height(50.dp)
+        )
+        Text (text = "Email")
+        TextField(
+            Modifier
+                .width(200.dp)
+                .height(30.dp)
+                .padding(25.dp)
+        )
+        Text (text = "Password")
+        TextField(
+            modifier = Modifier
+                .width(200.dp)
+                .height(30.dp)
+                .padding(25.dp)
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text("LogIn")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun TextField(modifier: Modifier) {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+
+        value = text,
+        onValueChange = { newText ->
+            text = newText
+        }
+    )
+}
+
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 800)
 @Composable
 fun DefaultPreview() {
     MDP7Theme {
-        Greeting("Android")
+        MyApp()
     }
 }

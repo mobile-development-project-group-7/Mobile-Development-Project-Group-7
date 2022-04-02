8e6cb4767c21ca351e46a5e857c77d1397b86059
Aliki Kapasakalidi <aliki02.kapasakalidi@gmail.com>
2022-04-02 15:07:37 +0300

    connected log in view to firebase email/password authentication

--- app/src/main/java/com/example/mdp7/MainActivity.kt
+++ app/src/main/java/com/example/mdp7/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -13,27 +14,64 @@ import com.example.mdp7.ui.theme.MDP7Theme
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.input.TextFieldValue
 
+
+import android.util.Log
+import android.util.Patterns
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import com.example.mdp7.MainActivity.Companion.TAG
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+
+    companion object{
+        val TAG: String = MainActivity::class.java.simpleName
+    }
+    private val auth by lazy{
+        Firebase.auth
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MDP7Theme {
-                MyApp()
+                LoginScreen(auth)
             }
         }
     }
 }
 
+
 @Composable
-fun MyApp() {
+fun LoginScreen(auth:FirebaseAuth){
+
+    val focusManager = LocalFocusManager.current
+
+    var email by remember{
+        mutableStateOf("")
+    }
+    var password by remember{
+        mutableStateOf("")
+    }
+
+    val isEmailValid by derivedStateOf {
+        Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    val isPasswordValid by derivedStateOf {
+        password.length > 5
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    ){
+
         Image(
             painter = painterResource(R.drawable.budgettracker),
             contentDescription = "Logo",
@@ -41,43 +79,58 @@ fun MyApp() {
                 .width(250.dp)
                 .height(50.dp)
         )
-        Text (text = "Email")
-        TextField(
-            Modifier
-                .width(200.dp)
-                .height(30.dp)
-                .padding(25.dp)
+        OutlinedTextField(value = email,
+            onValueChange = {email = it},
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {focusManager.moveFocus(FocusDirection.Down)}
+            ),
+            isError = !isEmailValid
         )
-        Text (text = "Password")
-        TextField(
-            modifier = Modifier
-                .width(200.dp)
-                .height(30.dp)
-                .padding(25.dp)
+        OutlinedTextField(value = password,
+            onValueChange = {password = it},
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {focusManager.clearFocus()}
+            ),
+            isError = !isPasswordValid
         )
-        Button(onClick = { /*TODO*/ }) {
-            Text("LogIn")
+
+        Button(onClick = {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Log.d(TAG, "SUCCESS")
+                    }else{
+                        Log.w(TAG, "FAILED", it.exception)
+                    }
+                }
+
+        },
+            enabled = isEmailValid && isPasswordValid) {
+
+            Text(text = "Log in")
         }
+
     }
 }
 
-@Composable
-fun TextField(modifier: Modifier) {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
-    TextField(
-
-        value = text,
-        onValueChange = { newText ->
-            text = newText
-        }
-    )
-}
 
 
 @Preview(showBackground = true, widthDp = 360, heightDp = 800)
 @Composable
 fun DefaultPreview() {
     MDP7Theme {
-        MyApp()
+        LoginScreen(Firebase.auth)
     }
 }

085e9ba38101519ca05de81b2d627fdc5cf73ae4
Aliki Kapasakalidi <aliki02.kapasakalidi@gmail.com>
2022-04-12 09:53:15 +0300

    preparing to merge registration to log in branch

--- app/src/main/java/com/example/mdp7/MainActivity.kt
+++ app/src/main/java/com/example/mdp7/MainActivity.kt
@@ -3,32 +3,54 @@ package com.example.mdp7
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mdp7.ui.theme.MDP7Theme
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+
+
+import android.util.Log
+import android.util.Patterns
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+
+    companion object{
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MDP7Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    RegistrationApp()
-                }
+                MainView()
             }
         }
     }
 }
 
 
+
+
+
 @Preview(showBackground = true, widthDp = 360, heightDp = 800)
 @Composable
 fun DefaultPreview() {
     MDP7Theme {
-        RegistrationApp()
+        MainView()
     }
 }
